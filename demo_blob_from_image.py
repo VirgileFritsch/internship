0b8b2a7038d7057c94470b73fd2b6695302b83e9
VirgileFritsch <virgile.fritsch@gmail.com>
2010-09-10 16:21:21 +0200

    Fixed bugs introduced by Bertrand's changes in nipy hroi module.

--- demo_blob_from_image.py
+++ demo_blob_from_image.py
@@ -10,13 +10,11 @@ Author : Virgile Fritsch, 2010,
          adapted from original Bertrand Thirion's script, 2009
 """
 
-import os.path as op
 import os
 import numpy as np
 from nipy.io.imageformats import load, save, Nifti1Image
-import tempfile
-import nipy.neurospin.graph.field as ff
 import nipy.neurospin.spatial_models.hroi as hroi
+from nipy.neurospin.spatial_models.discrete_domain import domain_from_image
 
 # -----------------------------------------------------------
 # --------- Paths and Parameters ----------------------------
@@ -34,6 +32,8 @@ if SUBJECT == "group":
     # output directory
     BLOBS3D_DIR = "%s/blobs3D_%s" % (m_path, CONTRAST)
 
+# path to the data mask
+#brain_mask_path = ?
 # path to data to extract the blobs from
 #glm_data_path = ?
 
@@ -46,81 +46,80 @@ if not os.path.exists(swd):
 # - "mblobs": mean-labelled blobs
 #BLOB3D_TYPE = ?
 
-# blob-forming threshold
-threshold = THETA3D
-# size threshold on bblobs
-smin = SMIN3D
+# blob-forming threshold (min significant intensity value)
+#THETA3D = ?
+# size threshold on blobs
+#SMIN3D = ?
 
 # -----------------------------------------------------------
 # --------- Script starts -----------------------------------
 # -----------------------------------------------------------
-# Prepare the data
+### Compute the nested roi object
+# get data domain (~ data mask)
+mask = load(brain_mask_path)
+mask_data = mask.get_data()
+domain = domain_from_image(mask)
+# get data
 nim = load(glm_data_path)
-affine = nim.get_affine()
-shape = nim.get_shape()
-data = nim.get_data()
-values = data[data!=0]
-xyz = np.array(np.where(data)).T
-F = ff.Field(xyz.shape[0])
-F.from_3d_grid(xyz)
-F.set_field(values)
+glm_data = nim.get_data()[mask_data != 0]
+# construct the blobs hierarchy
+nroi = hroi.HROI_as_discrete_domain_blobs(domain, glm_data.ravel(),
+                                          threshold=THETA3D, smin=SMIN3D)
 
-# Compute the nested roi object
-label = -np.ones(F.V)
-nroi = hroi.NROI_from_field(F, affine, shape, xyz, 0, threshold, smin)
-bmap = -np.zeros(F.V)
+#------------------------------------------------------------
+### Extract blobs maps as textures
+blobs_labels = -np.zeros(nroi.domain.size)
+blobs_means = -np.zeros(nroi.domain.size)
 if nroi != None:
-    nroi.set_discrete_feature_from_index('activation', values)
-    bfm = nroi.discrete_to_roi_features('activation')
-    idx = nroi.discrete_features['index']
+    nroi.make_feature('activation', glm_data.ravel())
+    bfm = nroi.representative_feature('activation')
     for k in range(nroi.k):
-        label[idx[k]] = k
-        bmap[idx[k]] = bfm[k]
-	
-# saving the blob image,i. e. a label image 
-wlabel = -2 * np.ones(shape)
-wlabel[data!=0] = label + 1
-wim = Nifti1Image(wlabel, affine)
-wim.get_header()['descrip'] = 'blob image extracted from %s' %glm_data_path 
-save(wim, op.join(swd,"blob.nii"))
-
+        blobs_labels[nroi.label == k] = k
+        blobs_means[nroi.label == k] = bfm[k]
+# saving the blobs image, i.e. a label image
+label_data = np.zeros(mask_data.shape)
+label_data[mask_data != 0] = (blobs_labels + 1)
+label_image = Nifti1Image(label_data, mask.get_affine())
+label_image.get_header()['descrip'] = 'blob image extracted from %s' \
+                                      %glm_data_path 
+save(label_image, os.path.join(swd,"blob.nii"))
+print "Wrote the blobs label image in %s" \
+      %os.path.join(swd, "blob.nii")
 # saving the image of the average-signal-per-blob
-wlabel = np.zeros(shape)
-wlabel[data!=0] = bmap
-wim = Nifti1Image(wlabel, affine)
-wim.get_header()['descrip'] = 'blob average signal extracted from %s' \
-                              %(glm_data_path) 
-save(wim, op.join(swd,"bmap.nii"))
+avg_data = np.zeros(mask_data.shape)
+avg_data[mask_data != 0] = blobs_means
+avg_image = Nifti1Image(avg_data, mask.get_affine())
+avg_image.get_header()['descrip'] = 'blob average signal extracted from %s' \
+                                      %glm_data_path 
+save(avg_image, os.path.join(swd,"bmap.nii"))
+print "Wrote the blobs average signal image in %s" \
+      %os.path.join(swd, "bmap.nii")
 
-# saving the image of the end blobs or leaves
-label = -np.ones(F.V)
-mean_value = -np.ones(F.V)
+#------------------------------------------------------------
+### Extract end-blobs (or leaves) maps as textures
+leaves_labels = -np.zeros(nroi.domain.size)
+leaves_means = -np.zeros(nroi.domain.size)
 if nroi != None:
-    nroi.set_discrete_feature_from_index('average', values)
     lroi = nroi.reduce_to_leaves()
-    idx = lroi.discrete_features['index']
-    leaves_avg = lroi.discrete_to_roi_features('average')
+    bfm = lroi.representative_feature('activation')
     for k in range(lroi.k):
-        label[idx[k]] = k
-        mean_value[idx[k]] =  leaves_avg[k,0]
-
-if BLOB3D_TYPE == "mblobs":  #mean-labelled blobs
-    wlabel = -2 * np.ones(shape)
-    wlabel[data!=0] = mean_value
-    wim = Nifti1Image(wlabel, affine)
-    wim.get_header()['descrip'] = 'blob image extracted from %s' %glm_data_path 
-    save(wim, op.join(swd,"mleaves.nii"))
-    
-    print "Wrote the blob image in %s" %op.join(swd, "blob.nii")
-    print "Wrote the blob-average signal image in %s" %op.join(swd, "bmap.nii")
-    print "Wrote the end-blob image in %s" %op.join(swd, "mleaves.nii")
-else:  #index-labelled blobs
-    wlabel = -2 * np.ones(shape)
-    wlabel[data!=0] = label + 1
-    wim = Nifti1Image(wlabel, affine)
-    wim.get_header()['descrip'] = 'blob image extracted from %s' %glm_data_path 
-    save(wim, op.join(swd,"leaves.nii"))
-    
-    print "Wrote the blob image in %s" %op.join(swd, "blob.nii")
-    print "Wrote the blob-average signal image in %s" %op.join(swd, "bmap.nii")
-    print "Wrote the end-blob image in %s" %op.join(swd, "leaves.nii")
+        leaves_labels[lroi.label == k] = k
+        leaves_means[lroi.label == k] = bfm[k]
+# saving the end-blobs image, i.e. a label image
+label_data = np.zeros(mask_data.shape)
+label_data[mask_data != 0] = (leaves_labels + 1)
+label_image = Nifti1Image(label_data, mask.get_affine())
+label_image.get_header()['descrip'] = 'blob image extracted from %s' \
+                                      %glm_data_path 
+save(label_image, os.path.join(swd,"leaves.nii"))
+print "Wrote the end-blobs label image in %s" \
+      %os.path.join(swd, "leaves.nii")
+# saving the end-blobs image of the average-signal-per-blob
+avg_data = np.zeros(mask_data.shape)
+avg_data[mask_data != 0] = leaves_means
+avg_image = Nifti1Image(avg_data, mask.get_affine())
+avg_image.get_header()['descrip'] = 'blob average signal extracted from %s' \
+                                      %glm_data_path 
+save(avg_image, os.path.join(swd,"mleaves.nii"))
+print "Wrote the end-blobs average signal image in %s" \
+      %os.path.join(swd, "mleaves.nii")
