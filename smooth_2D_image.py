e2354767e188d3c3ad6550533658341b41673820
VirgileFritsch <virgile.fritsch@gmail.com>
2010-09-13 17:21:32 +0200

    Made script for textures smoothing easier to use.

--- smooth_2D_image.py
+++ smooth_2D_image.py
@@ -17,28 +17,54 @@ import library_smoothing as smooth
 from nipy.neurospin.glm_files_layout import tio
 from gifti import loadImage
 
+# -----------------------------------------------------------
+# --------- Paths -------------------------------------------
+# -----------------------------------------------------------
 from database_archi import *
 
+# Path to the meshes
+#rmesh_path_gii = ?
+#lmesh_path_gii = ?
+
+# Path to the original textures
+#orig_rtex_path = ?
+#orig_ltex_path = ?
+
+# Output path for the smoothed images
+#smoothed_rtex_path = ?
+#smoothed_ltex_path = ?
+
+
+# -----------------------------------------------------------
+# --------- Parameters  -------------------------------------
+# -----------------------------------------------------------
+
+# Amount of (gaussian) smoothing
+#FWHM = ?
+
+# -----------------------------------------------------------
+# --------- Define some useful functions --------------------
+# -----------------------------------------------------------
 def get_edges_from_polygons(polygons, vertices):
-    """
+    """Builds a mesh edges list from its polygons and vertices.
 
     """
     nb_edges = 3 * polygons.shape[0]
     edges = np.zeros((nb_edges, 2))
     # get the polygons edges as tuples
-    permutator = np.array([(0,0,1),(1,0,0),(0,1,0)], dtype=int)
+    permut = np.array([(0,0,1),(1,0,0),(0,1,0)], dtype=int)
     edges[:,0] = np.ravel(polygons)
-    edges[:,1] = np.ravel(np.dot(polygons, permutator))
+    edges[:,1] = np.ravel(np.dot(polygons, permut))
     ind = np.lexsort((edges[:,1], edges[:,0]))
     edges = edges[ind]
 
     return edges
 
 # -----------------------------------------------------------
-# --------- Processing hemispheres separately ---------------
+# --------- Process hemispheres separately ------------------
 # -----------------------------------------------------------
 for hemisphere in ['left','right']:
-    print "Processing %s hemisphere:" %hemisphere
+    print "Smoothing: processing %s hemisphere:" %hemisphere
     sys.stdout.flush()
     if hemisphere == "right":
         mesh_path = rmesh_path_gii
@@ -53,25 +79,25 @@ for hemisphere in ['left','right']:
     # /!\ fixme : check that input_mesh is a triangular mesh
     print "  * Getting information from input mesh"
     sys.stdout.flush()
-    # load mesh
     input_mesh = loadImage(mesh_path)
-    # get vertices
-    c, n, t = input_mesh.getArrays()
+    input_mesh_arrays = input_mesh.getArrays()
+    if len(input_mesh_arrays) == 2:
+        c, t = input_mesh_arrays
+    elif len(input_mesh_arrays) == 3:
+        c, n, t = input_mesh_arrays
+    else:
+        raise ValueError("Error during gifti data extraction")
     vertices = c.getData()
     nb_vertices = vertices.shape[0]
-    # get polygons
     polygons = t.getData()
     nb_polygons = polygons.shape[0]
-    # get edges
     edges = get_edges_from_polygons(polygons, vertices)
     
     ### Get information from input texture
     # /!\ fixme : check that input_tex corresponds to the mesh
     print "  * Getting information from input texture"
     sys.stdout.flush()
-    # load texture
     input_tex = tio.Texture(orig_tex_path).read(orig_tex_path)
-    # get activation data
     activation_data = input_tex.data
     activation_data[np.isnan(activation_data)] = 0
         
@@ -87,16 +113,14 @@ for hemisphere in ['left','right']:
     N, dt = smooth.compute_smoothing_parameters(weights_matrix, FWHM)
         
     ### Apply smoothing
-    print "  * Smoothing...(FWHM = " + str(FWHM) + ")"
+    print "  * Smoothing...(FWHM = %g)" %FWHM
     sys.stdout.flush()
-    smoothed_activation_data = smooth.diffusion_smoothing(activation_data,
-                                                          LB_operator, N, dt)
+    smoothed_activation_data = smooth.diffusion_smoothing(
+        activation_data, LB_operator, N, dt)
     
     ### Write smoothed data into a new texture file
     print "  * Writing output texture"
     sys.stdout.flush()
-    # create a new texture object
     output_tex = tio.Texture(smoothed_tex_path, data=smoothed_activation_data)
-    # write the file
     output_tex.write()
 
